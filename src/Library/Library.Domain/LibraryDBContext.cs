ddac3948cd2502045a76d907747633a461d3eca7
Arnegil <i.arnegil@gmail.com>
2018-03-06 19:29:48 +0500

    Init

---
+++ src/Library/Library.Domain/LibraryDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Domain
+{
+    public class LibraryDBContext : DbContext
+    {
+        public LibraryDBContext(DbContextOptions<LibraryDBContext> options) : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/Library/Library.Services/Extensions/ConvertExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Services.Extensions
+{
+    public static class ConvertExtensions
+    {
+
+    }
+}
