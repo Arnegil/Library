5e44167f32f4f8b74b423b9a71dd8ffea4691636
Arnegil <i.arnegil@gmail.com>
2018-03-13 17:48:48 +0500

    Projects fix

--- src/PizzaDelivery/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
+++ src/PizzaDelivery/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
@@ -1,5 +1,5 @@
 /*!
-** Unobtrusive validation support PizzaDelivery for jQuery and jQuery Validate
+** Unobtrusive validation support library for jQuery and jQuery Validate
 ** Copyright (C) Microsoft Corporation. All rights reserved.
 */
 
