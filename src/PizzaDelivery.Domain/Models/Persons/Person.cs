4c28aa94855c0a930891e4ad328bf4d12efa52dc
LeafStrike <33098190+LeafStrike@users.noreply.github.com>
2018-04-21 18:42:51 +0500

    Add files via upload

--- src/PizzaDelivery.Domain/Models/Persons/Person.cs
+++ src/PizzaDelivery.Domain/Models/Persons/Person.cs
@@ -8,7 +8,7 @@ namespace PizzaDelivery.Domain.Models.Persons
         [Key]
         public Guid Id { get; set; }
 
-        [StringLength(50)]
+        /*[StringLength(50)]
         public string LastName { get; set; }
 
         [Required]
@@ -16,17 +16,40 @@ namespace PizzaDelivery.Domain.Models.Persons
         public string FirstName { get; set; }
 
         [StringLength(50)]
-        public string MiddleName { get; set; }
-        
+        public string MiddleName { get; set; }*/
+
+        [Required]
+        [StringLength(100)]
+        //[Display(Prompt ="ФИО")]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        //[Display(Description = "Логин")]
+        public string Login { get; set; }
+
+        //[Display(Description = "Дата рождения")]
         public DateTime? Birthday { get; set; }
 
         [Required]
         [DataType(DataType.PhoneNumber)]
+        //[Display(Description = "Тел. номер")]
         public string PhoneNumber { get; set; }
 
         [Required]
-        [StringLength(50)]
+        [StringLength(100)]
         [DataType(DataType.EmailAddress)]
+        //[Display(Description = "Эл. адрес")]
         public string Email { get; set; }
+
+        [StringLength(200)]
+        //[Display(Description = "Адрес доставки")]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        //[Display(Description = "Пароль")]
+        public string Password { get; set; }
     }
 }
