0d21eca5577ea0b4cf279791a6d4af9878e47b52
Arnegil <i.arnegil@gmail.com>
2018-05-10 21:20:11 +0500

    Ajax Lose, I Win

--- src/PizzaDelivery/wwwroot/js/site.js
+++ src/PizzaDelivery/wwwroot/js/site.js
@@ -1,16 +1,19 @@
 ﻿// Write your JavaScript code.
 
 function ajaxTest(dataM) {
-    var expectedData = '{"Pizza":{"Id":"00000000-0000-1111-0000-000000000001","Name":"Барбекю","Recipe":"Пицца с ветчиной, беконом, пепперони, болгарским перцем и томатным соусом для ценителей мясных деликатесов","Cost":410.0},"Count":1,"Sum":410.0}';
+    var id = dataM.find('input[name="orderPosition.Pizza.Id"]').val();
+    var name = dataM.find('input[name="orderPosition.Pizza.Name"]').val();
+    var recipe = dataM.find('input[name="orderPosition.Pizza.Recipe"]').val();
+    var cost = dataM.find('input[name="orderPosition.Pizza.Cost"]').val();
+    var count = dataM.find('input[name="orderPosition.Count"]').val();
 
-    var dataArray = dataM.serializeArray();
-    var dataJson = JSON.stringify(dataArray);
-    
+    var dataDto = '{"Pizza":{"Id":"' + id + '","Name":"' + name + '","Recipe":"' + recipe + '","Cost":' + cost + '},"Count":' + count + '}';
+
     $.ajax({
         type: "POST",
         url: '/Main/AddToShoppingCardAjax',
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        data: expectedData
+        data: dataDto
     });
 }
