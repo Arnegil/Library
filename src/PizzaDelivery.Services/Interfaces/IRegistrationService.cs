409f6e2fbb33e4cf0957636acd20750d0ce207b6
LeafStrike <33098190+LeafStrike@users.noreply.github.com>
2018-05-19 16:53:19 +0500

    Update IRegistrationService.cs

--- src/PizzaDelivery.Services/Interfaces/IRegistrationService.cs
+++ src/PizzaDelivery.Services/Interfaces/IRegistrationService.cs
@@ -8,5 +8,6 @@ namespace PizzaDelivery.Services.Interfaces
     public interface IRegistrationService
     {
         void RegisterPerson(Person person, Account account);
+        void RegisterEmployee(Person newPerson, Account newAccount, string role);
     }
 }
